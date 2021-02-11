83e4dd1eaf72cf3d8c046efa5d11a45df6ec5cd3
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-11 19:07:26 +0900

    [Add] make:following

---
+++ backend/app/Http/Controllers/FollowingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Following;
+use Illuminate\Http\Request;
+
+class FollowingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Following  $following
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Following $following)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Following  $following
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Following $following)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Following  $following
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Following $following)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Following  $following
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Following $following)
+    {
+        //
+    }
+}
