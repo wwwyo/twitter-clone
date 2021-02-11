ac5319acc6ee5f3a173f4381299baf3483d6b67b
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-11 14:06:35 +0900

    [Add] モデルのアソシエーション

--- backend/app/User.php
+++ backend/app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Post;
+use App\Comment;
 
 class User extends Authenticatable
 {
@@ -26,4 +28,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany('Post');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('Comment');
+    }
 }
