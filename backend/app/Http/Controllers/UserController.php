57717477cb09d493064d11d986ac20ef0f26dcd2
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-11 17:07:21 +0900

    [Add] ユーザー詳細ページ

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('users.show', ['user' => $user]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+}
