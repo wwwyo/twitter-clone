3394c53ef9c3408bfcc3011c9729aedfec18c28f
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-11 18:51:08 +0900

    [Add] comment コントローラー

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -3,39 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        //
+        $this->middleware('auth');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(Post $post, Request $request)
     {
-        //
-    }
+        $params = $request->validate([
+            'text' => 'required|max:300',
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $user_id = Auth::id();
+
+        $comment = new Comment();
+        $comment->text = $request->text;
+        $comment->user_id = $user_id;
+        $comment->post_id = $post->id;
+        $comment->save();
+        return redirect()->route('post.show', $comment->post);
     }
 
     /**
@@ -44,29 +36,7 @@ class CommentController extends Controller
      * @param  \App\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function show(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, Comment $comment)
     {
         //
