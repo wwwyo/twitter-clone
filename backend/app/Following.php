01dd63bdaef42b151282cfdde820e822b137a668
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-12 16:58:41 +0900

    [Add] followingモデル

--- backend/app/Following.php
+++ backend/app/Following.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Following extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'following_user_id',
+    ];
+
+    public function following_user()
+    {
+        return $this->belongsTo(User::class, 'following_user_id');
+    }
 }
