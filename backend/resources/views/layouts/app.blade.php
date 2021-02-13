b19759321d26f5873be90eabbf0f0c7ec40406d6
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-13 18:27:02 +0900

    [Change] 英語から日本語表記へ

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -33,11 +33,11 @@
             </div>
         @else
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="nav-link" href="{{ route('login') }}">ログイン</a>
             </li>
             @if (Route::has('register'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    <a class="nav-link" href="{{ route('register') }}">新規登録</a>
                 </li>
             @endif
         @endauth
