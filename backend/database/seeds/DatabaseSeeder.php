584071d207aaad36e5cbe3dae22bde2208928827
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-02-15 14:17:40 +0900

    [Add] seeder

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\User;
+use App\Post;
+use App\Comment;
+use App\Following;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class, 5)->create()->each(function ($user) {
+            factory(Comment::class, 3)->create(['user_id' => $user->id]);
+            factory(Following::class, 2)->create(['user_id' => $user->id]);
+        });
     }
 }
